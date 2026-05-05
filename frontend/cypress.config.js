ce52f7bc5cbbfa68a5ecc7acdbf6730d04821aed
Your New Name <43520443+elizondohdz@users.noreply.github.com>
2026-05-05 12:32:35 -0600

    feat: add reporting
    Generate Mochawesome HTML Reports after each run

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,9 +2,21 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: true, 
+    html: true, 
+    json: true
+  },
 
 
   e2e: {
-    baseUrl: 'http://localhost:5173'
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      return config;
+    }
   },
 });
